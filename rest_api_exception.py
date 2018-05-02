f3fbe5c0a80494374d417dccc7d70f17152859e5
Sundar Ramakrishnan <mokbat@gmail.com>
2018-05-01 23:44:24 -0400

    Adding rest api testing exception class

---
+++ rest_api_exception.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# Module Docstring
+"""
+REST API EXCEPTION CLASS
+
+This module would account for all possible
+exceptions for REST API testing.
+"""
+
+# Create a custom exception class
+class StringSpaceException(Exception):
+    """Custom exception to handle spaces in string"""
+    pass
