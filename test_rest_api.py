33ac0a40a95a1bfe40c5c4953489dc20b16cdf5c
Sundar Ramakrishnan <mokbat@gmail.com>
2018-05-02 23:08:07 -0400

    Added two test cases on sorting and movie name in sub str of another movie

--- test_rest_api.py
+++ test_rest_api.py
@@ -11,6 +11,7 @@ and additional requirements.
 
 # Python Standard Libraries
 import unittest
+from copy import deepcopy
 import requests
 
 # Python Addon Libraries
@@ -175,6 +176,74 @@ class MoviesRestApi(unittest.TestCase):
         # Per the requirement, validate the count is atleast ONE
         self.assertGreaterEqual(movie_titles_with_palindrome, 1, \
         msg="There should be at least 1 movie with a palindrome string in its title")
+
+    def test_movie_name_is_substr(self):
+        """
+        Requirements:
+        SPL-006: There are at least two movies in the database whose title contain
+                 the title of another movie. Example: movie id: 287757
+                 (Scooby-Doo Meets Dante), movie id: 404463 (Dante). This example
+                 shows one such set. The business requirement is that there are
+                 at least two such occurrences.
+        """
+        # Declare a variable for counting movie titles with substr occurances
+        movie_count_with_substr = 0
+
+        # Use List Comprehensions to build the list of titles
+        movie_list = [each["title"] for each in self.results]
+
+        # Iterate through each of the movie dict in the result list
+        for each_movie in movie_list:
+
+            # Use deepcopy to create a reference list
+            movie_list_new = deepcopy(movie_list)
+
+            # Remove the current movie title from reference list
+            movie_list_new.remove(each_movie)
+
+            # Iterate through each of the movie dict in the reference list
+            for each_remaining_movie in movie_list_new:
+
+                # Check if it is a substr in the movie from the reference list
+                if each_movie in each_remaining_movie:
+                    movie_count_with_substr += 1
+
+        # Per the validation requirement, check if there are at least TWO titles
+        self.assertLess(2, movie_count_with_substr, \
+        msg="There needs to be atleast two titles which have substr")
+
+
+    def test_sorting(self):
+        """
+        Requirements:
+        SPL-003: Sorting requirement.
+                 Rule #1 Movies with genre_ids == null should be first in response.
+                 Rule #2, if multiple movies have genre_ids == null,
+                 then sort by id (ascending). For movies that have non-null
+                 genre_ids, results should be sorted by id (ascending)
+        """
+        # Declare movie list for required list, null genre_id list and non null genre_id list
+        required_sorted_movie_list, genre_ids_with_null_list, genre_ids_not_null_list = [], [], []
+
+        # Iterate through each of the movies dicts in the result list
+        for each_movie in self.results:
+
+            # If genre_id is empty then add to null list
+            if each_movie["genre_ids"] == []:
+                genre_ids_with_null_list.append(each_movie)
+            else:
+                # If genre_id is not empty then add to not null list
+                genre_ids_not_null_list.append(each_movie)
+
+        # Sort each lists by id
+        genre_ids_with_null_list = sorted(genre_ids_with_null_list, key=lambda k: k['id'])
+        genre_ids_not_null_list = sorted(genre_ids_not_null_list, key=lambda k: k['id'])
+
+        # Place the null list before not null list
+        required_sorted_movie_list = genre_ids_with_null_list + genre_ids_not_null_list
+
+        # Validate the requirement order
+        self.assertListEqual(required_sorted_movie_list, self.results)
 
 if __name__ == "__main__":
     unittest.main()
